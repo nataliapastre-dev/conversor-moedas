df5d69d69458509f0e934ee61085e1159020252e
nataliapastre-dev <natalia.pastre10@gmail.com>
2025-11-16 14:55:46 -0300

    Projeto conversor-moedas

--- conversor-moedas/vite.config.js
+++ conversor-moedas/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ✅ ajuste importante para o GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/conversor-moedas/', 
+  base: '/conversor-moedas/'
 })
